7415a4a9d947765727aea066da41674a15a9bd15
Sam Kruglov <samkruglov@gmail.com>
2019-01-20 10:40:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Bank Service
+
+A Web Admin Portal for the whole bank. 
+Create customers, watch their transactions in real-time, and maybe something else. Work in progress.
+ 
+---
+
---
+++ src/test/kotlin/com/samkruglov/bank/ApplicationKtTest.kt
@@ -0,0 +1,13 @@
+package com.samkruglov.bank
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationKtTest {
+
+    @Test
+    fun `context loads`() {
+
+    }
+}
